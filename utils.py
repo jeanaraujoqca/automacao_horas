9d3abd4fbd2d72faf0b85dc17a0d4ff1708eaffa
Jean Araujo <jeanaraujo@queirozcavalcanti.adv.br>
2024-11-13 17:50:40 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 def bg_page(image_file):
     st.markdown(
-    f"""
+    """
     <style>
     .stApp {{
-        background-image: url("{https://raw.githubusercontent.com/jeanaraujoqca/automacao_horas/refs/heads/main/bg_dark.png})";
+        background-image: url("https://raw.githubusercontent.com/jeanaraujoqca/automacao_horas/refs/heads/main/bg_dark.png)";
         background-size: cover
     }}
     </style>
